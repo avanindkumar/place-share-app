e119d0ac1613cc9483b294dacfc89fca7ffd92ad
Avanind Parjapati <avanindkumar@gmail.com>
2022-11-20 00:47:40 +0530

    New Places form validation
    Done with new page form validation.

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import Map from "../../shared/components/UIElements/Map";
+import Modal from "../../shared/components/UIElements/Modal";
 
 import "./PlaceItem.css";
 const PlaceItem = ({
@@ -12,24 +14,44 @@ const PlaceItem = ({
   creatorId,
   coordinates,
 }) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
   return (
-    <li className="place-item">
-      <Card className="palce-item__content">
-        <div className="place-item__image">
-          <img src={image} alt={title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className="map-container">
+          <Map center={coordinates} zoom={16} />
         </div>
-        <div className="place-item__info">
-          <h2>{title}</h2>
-          <h3>{address} </h3>
-          <p>{description} </p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>View On Map</Button>
-          <Button to={`/palces/${id}`}>Edit</Button>
-          <Button danger>Delete</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="palce-item__content">
+          <div className="place-item__image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{title}</h2>
+            <h3>{address} </h3>
+            <p>{description} </p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              View On Map
+            </Button>
+            <Button to={`/palces/${id}`}>Edit</Button>
+            <Button danger>Delete</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
