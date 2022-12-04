448a2865b7e726bb196a182df957498213da068a
Avanind Parjapati <avanindkumar@gmail.com>
2022-12-04 19:59:29 +0530

    Updates

---
+++ precache-manifest.427e7b2cd589ec03a00cd4a72b68a424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4ee6ea30dff8a839dc4c6927811550",
+    "url": "/place-share-app/index.html"
+  },
+  {
+    "revision": "7560d5403ab9b891523d",
+    "url": "/place-share-app/static/css/main.226e857d.chunk.css"
+  },
+  {
+    "revision": "686bea02bd60538d10a3",
+    "url": "/place-share-app/static/js/2.2c1c3ef6.chunk.js"
+  },
+  {
+    "revision": "7560d5403ab9b891523d",
+    "url": "/place-share-app/static/js/main.98e04130.chunk.js"
+  },
+  {
+    "revision": "9cb0d83de623f9000c3e",
+    "url": "/place-share-app/static/js/runtime-main.4c985d78.js"
+  }
+]);
